b0735308d47504fe62f3d4832cb6c872b37f86e3
Fabio Santos <101347552+fabioaes@users.noreply.github.com>
2022-12-05 08:07:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ const btn = document.getElementById("enter");
 
 login.addEventListener("keyup", () => {
   if (login.value.length <= 7) {
-    img.setAttribute("src", "/img/1.png");
+    img.setAttribute("src", "https://raw.githubusercontent.com/fabioaes/Tela-de-Login-Animada/main/img/1.png");
   }
   if (login.value.length >= 8) {
-    img.setAttribute("src", "/img/2.png");
+    img.setAttribute("src", "https://raw.githubusercontent.com/fabioaes/Tela-de-Login-Animada/main/img/2.png");
   }
   if (login.value.length >= 13) {
-    img.setAttribute("src", "/img/3.png");
+    img.setAttribute("src", "https://raw.githubusercontent.com/fabioaes/Tela-de-Login-Animada/main/img/3.png");
   }
 });
